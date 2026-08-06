3ad6f9aad81c28bdab246f520fdae7f2f241bdb3
Harley <harleyhowell4@gmail.com>
2026-08-06 18:08:48 -0500

    Update blog.tsx

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,4 +1,4 @@
-import PostCard from "../components/Postcard";
+import PostCard from "app/components/Postcard";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
